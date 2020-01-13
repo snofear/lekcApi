dd684508983e36331a3045d3d0aca73d1dc91ae2
Nik Hrovat <nikhrovat@hotmail.com>
2020-01-13 18:23:53 +0100

    Heroku popravki

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template
 import requests
+from random import randint
+import os
 
 app = Flask(__name__)
 
-f = open("API_KEY.txt", "r")
-API_KEY = f.read()
+try:
+    API_KEY = os.environ["API_KEY"]
+except:
+    API_KEY = open("API_KEY.txt").read().strip()
 
-URL = f"https://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}"
+
+ime_mesta = ["Ljubljana", "London", "Paris", "Berlin"][randint(0,3)]
 
 @app.route("/", methods=["GET"])
 def index():
-    result = requests.get(URL)
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={ime_mesta}&appid={API_KEY}&units=metric"
+    result = requests.get(URL).json()
     return render_template("index.html", data=result)
 
 if __name__ == "__main__":
